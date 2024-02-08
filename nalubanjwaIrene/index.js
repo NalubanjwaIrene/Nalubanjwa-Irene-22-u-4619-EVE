88b7008018a8837234dece901d26c7f70cf043e4
NalubanjwaIrene <149466378+NalubanjwaIrene@users.noreply.github.com>
2024-02-08 11:47:28 +0300

    Update index.js

--- nalubanjwaIrene/index.js
+++ nalubanjwaIrene/index.js
@@ -1,21 +1,24 @@
 // Declaring two variables ie email and password
-let email = "nalubanjwairene@gmail.com";
-let password = "22/u/4616/EVE@2024";
+let email = "nalubanjwairene@gmail.com"
+let password = "22/u/4616/EVE@2024"
 
 // declaring the prompt form for entering credentials
-let studentEmail = prompt("Enter your email");
-let studentPassword = prompt("Enter your Password");
+let userEmail = prompt("Enter your email")
+let userPassword = prompt("Enter your Password")
 
 // Introducing the javascript function with two parameters
-function loginCredentials(validEmail, validPassword){
-    if(validEmail === email && validPassword === password){
-        console.log(`"User email is [email].login succcessfully!" ${studentPassword}`)
+function loginCredentials(inputEmail, inputPassword){
+    if(inputEmail === email && inputPassword === password){
+        console.log(`"User email is [email].login succcessfully!" ${userPassword}`)
     }else{
-        console.log(`The provided email : ${studentEmail} Incorrect user credentials`)
+        console.log(`The provided email : ${userEmail} Incorrect user credentials`)
     }
 }
 
 // Calling the function
-loginCredentials(studentEmail, studentPassword);
+loginCredentials(userEmail, userPassword)
+
+
+
 
 
